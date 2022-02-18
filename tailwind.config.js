d52bcead89d620083b644962bb59616dba3d5c7f
syf107 <syariefarifin107@gmail.com>
2022-02-18 23:59:54 +0700

    second commit to flexbox tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        mammoth: "10rem",
+      },
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
   },
   plugins: [],
 };
